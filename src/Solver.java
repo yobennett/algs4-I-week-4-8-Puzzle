75ac2b9957b30a70bc756da40d75ee047ad9ff2c
Bennett <bennett@yobennett.com>
2015-04-04 13:46:39 -0700

    refactor Solver to use while loop with flag

--- src/Solver.java
+++ src/Solver.java
@@ -8,108 +8,105 @@ public class Solver {
     private static final int UNSOLVABLE = -1;
     private final Board initial;
     private Queue<Board> solution;
-    private MinPQ<SearchNode> pq;
+    private MinPQ<SearchNode> minPQ;
+    private int moves = 0;
     private int enqueues = 0;
     private int dequeues = 0;
+    private boolean solvable = false;
+    private SearchNode finalNode;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null)
             throw new NullPointerException();
         this.initial = initial;
-        this.pq = new MinPQ<SearchNode>(new ByManhattanPriority());
+        this.minPQ = new MinPQ<SearchNode>(new ByManhattanPriority());
         this.solution = new Queue<Board>();
+
+        enqueueSearchNode(new SearchNode(initial, 0, null));
         solve();
     }
+
+    private void solve() {
+        while (!isSolvable()) {
+            SearchNode node = dequeueMinSearchNode();
+            if (node.board.isGoal()) {
+                solvable = true;
+                finalNode = node;
+            } else {
+                for (Board neighbor : node.board.neighbors()) {
+                    if (node.previousNode != null) {
+                        if (!neighbor.equals(node.previousNode.board))
+                            enqueueSearchNode(new SearchNode(neighbor, node.moves + 1, node));
+                    } else {
+                        enqueueSearchNode(new SearchNode(neighbor, node.moves + 1, node));
+                    }
+                }
+            }
+
+        }
+    }
+
 
     private void enqueueSearchNode(SearchNode node) {
 //        System.out.println("* ENQUEUE");
         enqueues++;
-        pq.insert(node);
+        minPQ.insert(node);
     }
 
     private SearchNode dequeueMinSearchNode() {
 //        System.out.println("** DEQUEUE");
         dequeues++;
-        return pq.delMin();
-    }
-
-    private void addSolutionStep(Board board) {
-        solution.enqueue(board);
-    }
-
-    private void solve() {
-        SearchNode node = new SearchNode(initial, 0, null);
-
-        enqueueSearchNode(node);
-
-        SearchNode min = dequeueMinSearchNode();
-        addSolutionStep(min.board());
-
-        if (!min.board().isGoal()) {
-            solve(min);
-        }
-    }
-
-    private void solve(SearchNode node) {
-        Board board = node.board();
-        Board prev = (node.previousNode() != null) ? node.previousNode().board() : null;
-
-        // add neighbors to priority queue
-        Iterable<Board> neighbors = board.neighbors();
-        for (Board neighbor : neighbors) {
-            if (!neighbor.equals(prev)) {
-                enqueueSearchNode(new SearchNode(neighbor, moves(), node));
-            }
-        }
-
-//        printState();
-
-        SearchNode min = dequeueMinSearchNode();
-        addSolutionStep(min.board());
-
-        if (!min.board().isGoal()) {
-            solve(min);
-        }
+        return minPQ.delMin();
     }
 
     private void printState() {
         StdOut.printf("Step %d:\n", moves());
-        //System.out.printf("%-23s %s %f %n", "mean", "=", stats.mean());
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return true;
+        return solvable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        if (!isSolvable()) {
-            return UNSOLVABLE;
+        if (isSolvable() && finalNode != null) {
+            return finalNode.moves;
         }
-        return solution.size() - 1;
+        return UNSOLVABLE;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return solution;
+        if (!isSolvable())
+            return null;
+        else {
+            Stack<Board> result = new Stack<Board>();
+            SearchNode node = finalNode;
+            while (node != null) {
+                result.push(node.board);
+                node = node.previousNode;
+            }
+            moves = result.size();
+            return result;
+        }
     }
 
     private class SearchNode {
 
         private final Board board;
-        private final int previousMoves;
+        private final int moves;
         private final int manhattan;
+        private final int hamming;
         private final SearchNode previousNode;
-        private final int priority;
 
-        SearchNode(Board board, int previousMoves, SearchNode previousNode) {
+        SearchNode(Board board, int moves, SearchNode previousNode) {
             this.board = board;
-            this.previousMoves = previousMoves;
+            this.moves = moves;
             this.manhattan = board.manhattan();
+            this.hamming = board.hamming();
             this.previousNode = previousNode;
-            this.priority = manhattan + previousMoves;
         }
 
         public Board board() {
@@ -117,7 +114,11 @@ public class Solver {
         }
 
         public int priority() {
-            return priority;
+            return manhattan + moves;
+        }
+
+        public int hammingPriority() {
+            return hamming + moves;
         }
 
         public SearchNode previousNode() {
@@ -126,9 +127,10 @@ public class Solver {
 
         @Override
         public String toString() {
-            String result = "priority = " + priority;
-            result += "\nmoves = " + previousMoves;
+            String result = "priority = " + priority();
+            result += "\nmoves = " + moves;
             result += "\nmanhattan = " + manhattan;
+            result += "\nhamming = " + hamming;
             result += "\n" + board.toString();
             return result;
         }
@@ -138,15 +140,7 @@ public class Solver {
     private class ByManhattanPriority implements Comparator<SearchNode> {
         @Override
         public int compare(SearchNode node1, SearchNode node2) {
-            int priority1 = node1.priority();
-            int priority2 = node2.priority();
-            if (priority1 > priority2) {
-                return 1;
-            } else if (priority1 < priority2) {
-                return -1;
-            } else {
-                return 0;
-            }
+            return node1.priority() - node2.priority();
         }
     }
 
@@ -170,7 +164,7 @@ public class Solver {
             StdOut.println("No solution possible");
         else {
             StdOut.println("Minimum number of moves = " + solver.moves());
-            StdOut.println("enqueues = " + solver.enqueues + ", dequeues = " + solver.dequeues);
+//            StdOut.println("enqueues = " + solver.enqueues + ", dequeues = " + solver.dequeues);
             for (Board board : solver.solution())
                 StdOut.println(board);
         }
